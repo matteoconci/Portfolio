bc768533a68ff8c5ae5178e289fd822459c59be5
Matteo Conci <matteo.conci54@hotmail.it>
2025-02-18 21:21:06 +0100

    Add last JavaScript and adjust some line

--- main.js
+++ main.js
@@ -6,19 +6,20 @@ const aboutMeDisplay = () => {
     aboutMe.style.display = 'block';
     project.style.display = 'none';
     contact.style.display = 'none';
-}
+};
 
 const projectDisplay = () => {
     aboutMe.style.display = 'none';
     project.style.display = 'block';
     contact.style.display = 'none';
-}
+};
 
 const contactDisplay = () => {
     aboutMe.style.display = 'none';
     project.style.display = 'none';
     contact.style.display = 'block';
-}
+};
+
 
 const projectFlexbox = document.getElementById('flexbox');
 const projectTwo = document.getElementById('two');
@@ -28,29 +29,22 @@ const liFlexbox = document.querySelector('.project-flexbox');
 const liTwo = document.querySelector('.project-2');
 const liThree = document.querySelector('.project-3');
 
+
+
 const flexboxDisplay = () => {
     projectFlexbox.style.display = 'block';
-    liFlexbox.style.textDecoration = 'underline';
     projectTwo.style.display = 'none';
-    liTwo.style.textDecoration = 'none';
     projectThree.style.display = 'none';
-    liThree.style.textDecoration = 'none';
-}
+};
 
 const twoDisplay = () => {
     projectFlexbox.style.display = 'none';
-    liFlexbox.style.textDecoration = 'none';
     projectTwo.style.display = 'block';
-    liTwo.style.textDecoration = 'underline';
     projectThree.style.display = 'none';
-    liThree.style.textDecoration = 'none';
-}
+};
 
 const threeDisplay = () => {
     projectFlexbox.style.display = 'none';
-    liFlexbox.style.textDecoration = 'none';
     projectTwo.style.display = 'none';
-    liTwo.style.textDecoration = 'none';
     projectThree.style.display = 'block';
-    liThree.style.textDecoration = 'underline';
-}
+};
